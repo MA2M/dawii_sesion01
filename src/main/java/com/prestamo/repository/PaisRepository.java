cc3df08a51379a460740221eb204186d8a8709e0
MA2M <i202312923@cibertec.edu.pe>
2024-09-22 01:38:08 -0500

    prueba 0131

--- src/main/java/com/prestamo/repository/PaisRepository.java
+++ src/main/java/com/prestamo/repository/PaisRepository.java
@@ -2,7 +2,7 @@ package com.prestamo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.prestamo.entity.Pais;
-// TEST 0130
+// TEST 0131
 public interface PaisRepository extends JpaRepository<Pais, Integer> {
 
 }
